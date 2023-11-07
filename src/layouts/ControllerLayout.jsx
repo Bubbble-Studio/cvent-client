e66f7682f8669306678fae2a5a3b358503f565c9
Rishi <grishi634@gmail.com>
2023-11-07 14:14:27 +0530

    video added in bg

--- src/layouts/ControllerLayout.jsx
+++ src/layouts/ControllerLayout.jsx
@@ -1,7 +1,17 @@
 import styles from "./ControllerLayout.module.css";
 
 function Controller(props) {
-  return <div className={styles.controller}>{props.children}</div>;
+  return (
+    <>
+      <div className={styles.controller}>{props.children}</div>
+      <video
+        src="/assets/video/IMEX_BG.mp4"
+        autoPlay
+        muted
+        className={styles.BGvideo}
+      ></video>
+    </>
+  );
 }
 
 export default Controller;
